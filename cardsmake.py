f03a1b231d9009fdd32bb1d132d723a84e8f9938
illAlwayswatching <2104962413@qq.com>
2025-03-28 10:52:17 +0800

    卡片预制1.1 add name

--- cardsmake.py
+++ cardsmake.py
@@ -1,8 +1,24 @@
 from PIL import Image, ImageDraw, ImageFont
 
-def add_rounded_border(image_path, border_thickness=30, column_width=35, corner_radius=30):
+def add_name(image_path, name):
     # 打开图片
     base_image = Image.open(image_path).convert("RGBA")
+    width, height = base_image.size
+
+    texture = Image.open("assets/textures/card_name_texture.jpg").resize((width, 80))    
+    font = ImageFont.truetype("assets/fonts/arial.ttf", 36)
+
+    # 创建绘图对象
+    base_image.paste(texture, (0, 0))
+    draw = ImageDraw.Draw(base_image)
+    draw.text((50, 30), name, font=font, fill="black")
+    
+    return base_image
+
+
+def add_rounded_border(base_image, border_thickness=30, column_width=35, corner_radius=30):
+    # 打开图片
+    base_image = base_image.convert("RGBA")
     width, height = base_image.size
     
     # 创建绘图对象
@@ -29,7 +45,13 @@ def add_rounded_border(image_path, border_thickness=30, column_width=35, corner_
     return base_image
 
 # 使用示例
-image_path = "assets/rawpic/修行.jpg"
-image_with_border = add_rounded_border(image_path)
+# image_path = "assets/rawpic/修行.jpg"
+# base_image = Image.open(image_path).convert("RGBA")
+# width, height = base_image.size
+# print(
+#     f"width: {width}, height: {height}"
+# )
+image_with_border = add_name("assets/rawpic/修行0.jpg", "修行")
+image_with_border = add_rounded_border(image_with_border)
 image_with_border.show()
 image_with_border.save("assets/cards/修行.png")
